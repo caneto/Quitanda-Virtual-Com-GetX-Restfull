8d1785971d1a028fc485029901ebdfaabfe3e4fd
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-09-27 13:01:56 -0300

    Atualização do Auth Controller

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,5 +1,16 @@
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
-  
+
+  RxBool isLoading = false.obs;
+
+  Future<void> SignIn({required String email, required String password}) async {
+    isLoading.value = true;
+    
+    await Future.delayed(const Duration(seconds: 2));
+
+    isLoading.value = false;
+  }
+
+
 }
