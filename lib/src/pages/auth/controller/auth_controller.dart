2c3adf0431c0ef110a2426310665806354fdf9e0
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-09-30 11:54:31 -0300

    Atualização

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,11 +1,17 @@
 import 'package:get/get.dart';
+import 'package:quitandavirtual/src/models/user_model.dart';
 import 'package:quitandavirtual/src/pages/auth/repository/auth_repository.dart';
 import 'package:quitandavirtual/src/pages/auth/result/auth_result.dart';
+import 'package:quitandavirtual/src/pages_route/app_pages.dart';
+import 'package:quitandavirtual/src/utils/utils_services.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
 
   final authRepository = AuthRepository();
+  final utilsServices = UtilsServices();
+
+  UserModel user = UserModel();
 
   Future<void> signIn({
     required String email,
@@ -19,10 +25,14 @@ class AuthController extends GetxController {
 
     result.when(
       success: (user) {
-        print(user);
+        this.user = user;
+        Get.offAllNamed(PagesRoutes.homeRoute);
       },
       error: (message) {
-        print(message);
+        utilsServices.showToast(
+          message: message,
+          isError: true,
+        );
       },
     );
   }
